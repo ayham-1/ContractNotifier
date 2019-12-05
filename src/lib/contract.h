90df55f3e7bfbd9cd364c7c10cd85f823433ce95
altffour <56314286+realaltffour@users.noreply.github.com>
2019-12-05 18:02:34 +0300

    Add date manegment

--- src/lib/contract.h
+++ src/lib/contract.h
@@ -2,14 +2,37 @@
 #define CONTRACT_H
 
 #include <string>
+#include <ctime>
 #include <QDate>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+#define DATE_FORMAT "dd/MM/yyyy"
 
 struct Contract {
-    std::string _name;
-    std::string _desc;
-    QDate _expiry;
-    int _notify_from_months;
-    int _notify_from_days;
+    std::string _name = "";
+    std::string _desc = "";
+    std::string _expiry = "";
+    int _notify_from_months = 0;
+    int _notify_from_days = 0;
+
+    template<typename Archive>
+    void serialize(Archive& ar, const unsigned int version) {
+        ar & _name;
+        ar & _desc;
+        ar & _expiry;
+        ar & _notify_from_months;
+        ar & _notify_from_days;
+    }
 };
+
+auto contract_getExpiry(const Contract &contract) -> QDate {
+    return (QDate::fromString(QString::fromStdString(contract._expiry), DATE_FORMAT));
+}
+
+auto contract_setExpiry(Contract &contract, QDate date) -> void {
+    contract._expiry = date.toString(DATE_FORMAT).toUtf8().constData();
+}
+
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include "lib/contract.h"
+#include <iostream>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+    Contract contract;
+    QDate date;
+    date = QDate::currentDate();
+    contract_setExpiry(contract, date);
+    QDate date1;
+    date1 = contract_getExpiry(contract);
+
+    std::cout << contract._expiry;
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
